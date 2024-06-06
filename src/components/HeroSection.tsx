797a97ae5faecec1317d5ac083bc4ba0e9f1c979
chestamehta <e22cseu1283@bennett.edu.in>
2024-06-06 15:24:07 +0530

    moving button

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link"
+import { Spotlight } from "./ui/Spotlight"
+import { cn } from "@/utils/cn"
+import { Button } from "./ui/moving-border";
 
 
 function HeroSection() {
@@ -7,10 +10,21 @@ function HeroSection() {
     className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
     >
     <div className="p-4 relative z-10 w-full text-center" >
-        <h1>master the art of music</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum iste officiis pariatur maxime neque nam ut ratione quam ipsam nostrum deserunt consectetur id, adipisci quibusdam distinctio aliquid accusantium recusandae assumenda eius dignissimos! Aliquam, optio, itaque facere praesentium iusto dolor sed ad tempore voluptatibus vel veniam? Repellendus suscipit, nulla libero inventore, perspiciatis natus minus numquam est perferendis dignissimos exercitationem eveniet repellat quibusdam sequi magni modi dicta aliquam, enim itaque vel. Repudiandae quis obcaecati quod totam. Laborum reiciendis commodi maxime! Quae repellendus, reprehenderit tempore delectus laudantium numquam dolor nesciunt corrupti, facilis perferendis, iste architecto? Accusamus assumenda quod ea ex, nemo earum alias.</p>
+    <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">master the art of music</h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto" >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam ea in laboriosam id ex mollitia libero corporis! Optio sapiente suscipit omnis. Ad velit enim impedit aliquam, modi illum! Numquam perspiciatis delectus voluptas hic laborum quos voluptatibus sapiente eius veniam minus, quis harum, molestias ex laudantium accusamus necessitatibus. Optio, similique et!</p>
         <div className="mt-4" >
-        <Link href={"/courses"}>explore courses</Link>
+        <Link href={"/courses"}>
+          <Button
+          borderRadius="1.75rem"
+          className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+          >
+            explore courses
+          </Button>
+        </Link>
         </div>
     </div>
     </div>
